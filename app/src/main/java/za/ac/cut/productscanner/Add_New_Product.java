4d4c1f777cc27a007921b14ab7d08f94f9c3947a
Shahbaaz <shabibo241995@gmail.com>
2016-08-19 16:18:40 +0200

    Last Updates for 19-08-2016 Pls commned Odd Error in AndroidManifest.xml Pls Halp

--- app/src/main/java/za/ac/cut/productscanner/Add_New_Product.java
+++ app/src/main/java/za/ac/cut/productscanner/Add_New_Product.java
@@ -3,14 +3,36 @@ package za.ac.cut.productscanner;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.zxing.integration.android.IntentIntegrator;
 
 public class Add_New_Product extends AppCompatActivity {
 
-    ActionBar actionaBar;
+    EditText et_scan, et_title, et_description;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add__new__product);
-        actionaBar.setTitle("Add Product");
+        getSupportActionBar().setTitle("Add Product");
+        et_scan =(EditText)findViewById(R.id.et_scan);
+        et_title = (EditText)findViewById(R.id.et_title);
+        et_description = (EditText)findViewById(R.id.et_description);
+
+
+    }
+    public void onClick_et_scan(View v){
+        scanNow(v);
+
+    }
+    public void scanNow(View view) {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
+        integrator.setPrompt("Scan the barcode or back to exit"); // text at bottom when scanner runs
+        integrator.setResultDisplayDuration(0);
+        integrator.setWide();  // Wide scanning rectangle, may work better for 1D barcodes
+        integrator.setCameraId(0);  // Use a specific camera of the device
+        integrator.initiateScan();
     }
 }
--- app/src/main/java/za/ac/cut/productscanner/MainActivity.java
+++ app/src/main/java/za/ac/cut/productscanner/MainActivity.java
@@ -1,10 +1,12 @@
 package za.ac.cut.productscanner;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -19,5 +21,18 @@ public class MainActivity extends ActionBarActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.option_buttons, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_products:
+                startActivity(new Intent(MainActivity.this,Add_New_Product.class));
+                return true;
+            case R.id.sync_products:
+
+                return true;
+            default:return super.onOptionsItemSelected(item);
+        }
     }
 }
